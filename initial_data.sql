fe34678b613f866c1638e84a9b341c89a411fc50
romaimperator <romaimperator@gmail.com>
2010-12-24 15:31:14 -0600

    changed the password

--- initial_data.sql
+++ initial_data.sql
@@ -11,7 +11,7 @@ INSERT INTO aros (model, foreign_key, alias, lft, rght) VALUES ('User', 1, NULL,
 
 INSERT INTO aros_acos (aro_id, aco_id, _create, _read, _update, _delete) VALUES (1, 1, 1, 1, 1, 1);
 
-INSERT INTO users (username, password, email, group_id, created, modified) VALUES ('admin', '442dbbc8dc23a4e1a121581bab9e4390a72e2d14', 'admin@gmail.com', 1, now(), now());
+INSERT INTO users (username, password, email, group_id, created, modified) VALUES ('admin', '1971b78e75ba6d94aaef66ce66aa87336ee760b9', 'admin@gmail.com', 1, now(), now());
 INSERT INTO users (username, password, email, group_id, created, modified) VALUES ('broseph', 'passwordhashthatihaventimplementedyet', 'broseph@gmail.com', 3, now(), now());
 
 INSERT INTO friends (user_id, friend_id, created, modified) VALUES (1, 2, now(), now());
