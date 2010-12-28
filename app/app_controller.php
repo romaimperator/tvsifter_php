9cbf71f2e95b8430c35a723f04cd59df4d3ee061
romaimperator <romaimperator@gmail.com>
2010-12-28 12:58:19 -0600

    fixed adding the cache helper in app controller

--- app/app_controller.php
+++ app/app_controller.php
@@ -3,17 +3,18 @@
 class AppController extends Controller {
     var $helpers = array(
         'Asset.asset',// => array('debug' => TRUE),
-        'Session',
-        'Html',
-        'Form',
-        'Javascript',
+        'Cache',
         'ElementCombiner.elementcombiner',
+        'Form',
+        'Html',
+        'Session',
+        'Javascript',
     );
 
     var $components = array(
-        'DebugKit.Toolbar' => array('panels' => array('Interactive.interactive')),
         'Acl',
         'Auth',
+        'DebugKit.Toolbar' => array('panels' => array('Interactive.interactive')),
         'Session',
     );
 
