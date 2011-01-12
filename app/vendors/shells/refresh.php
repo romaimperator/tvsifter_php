d8435907ffa33f8eeb75d5120438214a7f5ab400
romaimperator <romaimperator@gmail.com>
2011-01-12 14:16:51 -0600

    renamed and removed old file

--- app/vendors/shells/refresh.php
+++ app/vendors/shells/refresh.php
@@ -1,5 +1,7 @@
 <?php
 
+App::import('Core', 'Security');
+
 class RefreshShell extends Shell {
     var $uses = array('Show'); 
     var $tasks = array('Daemon'); 
@@ -7,8 +9,11 @@ class RefreshShell extends Shell {
     function refresh() { 
         $this->Daemon->execute('refresh'); 
         //Configure::write('debug', 0); 
-        $this->out('Beginning refresh...'); 
+        $start = time();
+        $this->out(date('Y-m-d H:i:s', time()).'Beginning refresh...'); 
         $this->Show->refresh_all();
-        $this->out('Ending refresh...');
+        $this->out(date('Y-m-d H:i:s', time()).'Ending refresh...');
+        $end = time();
+        $this->out('Update took '.$end-$start.' seconds to complete.');
     } 
 }
